915507945536e9ea3c5e6b56b9cfc8806aa28baa
mlatifr <mlatifraldi@gmail.com>
2021-05-29 11:26:46 +0700

    conn to con

--- local/addmovie.php
+++ local/addmovie.php
@@ -3,16 +3,16 @@
 extract($_POST);
 $sql = "INSERT INTO movie_genres(movie_id,genre_id)
     VALUES(?,?)";
-$stmt = $conn->prepare($sql);
+$stmt = $con->prepare($sql);
 $stmt->bind_param("dd", $movie_id, $genre_id);
 $stmt->execute();
 if ($stmt->affected_rows > 0) {
-    $arr = ["result" => "success", "id" => $conn->insert_id];
+    $arr = ["result" => "success", "id" => $con->insert_id];
 } else {
-    $arr = ["result" => "fail", "Error" => $conn->error];
+    $arr = ["result" => "fail", "Error" => $con->error];
 }
 echo json_encode($arr);
 
-$conn->close();
+$con->close();
 
 ?>
