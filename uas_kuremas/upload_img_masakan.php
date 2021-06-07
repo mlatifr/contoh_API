2d6d97ba8e09635d9e907d04427f4318ed4f7d24
mlatifr <mlatifraldi@gmail.com>
2021-06-07 08:47:41 +0000

    hb upld img, lsg input ke db loksi img nya

--- uas_kuremas/upload_img_masakan.php
+++ uas_kuremas/upload_img_masakan.php
@@ -1,14 +1,37 @@
+<?php require 'connect.php';
+?>
 <?php
-header('Access-Control-Allow-Origin: *'); 
+header('Access-Control-Allow-Origin: *');
 $nama_masakan = $_POST['nama_masakan'];
 $img = base64_decode($_POST['img']);
-if(file_put_contents("images/".$nama_masakan.".jpg", $img))
-{
-    echo "Sukses upload file";
-}
-else
-{
+if (file_put_contents("images/" . $nama_masakan . ".jpg", $img)) {
+    // echo "Sukses upload file";
+    $imgLocation = "images/" . $nama_masakan . ".jpg";
+    $sql = "INSERT INTO masakan(nama,url_foto)
+    VALUES(?,?)";
+    $stmt = $con->prepare($sql);
+    $stmt->bind_param("ss", $nama_masakan, $imgLocation);
+    $stmt->execute();
+    if ($stmt->affected_rows > 0) {
+        $arr = ["result" => "success", "id" => $con->insert_id];
+    } else {
+        $arr = ["result" => "fail", "Error" => $con->error];
+    }
+    echo json_encode($arr);
+
+    $con->close();
+
+    // if ($result->num_rows > 0) {
+    //     while ($r = mysqli_fetch_assoc($result)) {
+    //         array_push($data, $r);
+    //     }
+    //     $arr = ["result" => "success", "data" => $data];
+    // } else {
+    //     $arr = ["result" => "error", "message" => "sql error: $sql"];
+    // }
+
+} else {
     echo "Gagal upload file";
 }
-	
+
 ?>
