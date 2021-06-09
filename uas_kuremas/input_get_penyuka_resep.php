563f83ac3a3cf2d4903d7c0e1aa8900b00d151ba
mlatifr <mlatifraldi@gmail.com>
2021-06-09 12:29:19 +0700

    kirim like ke database

--- uas_kuremas/input_get_penyuka_resep.php
+++ uas_kuremas/input_get_penyuka_resep.php
@@ -4,10 +4,10 @@
 
 extract($_POST);
 $sql = "INSERT INTO `resep_has_like` 
-(`resep_masakan_id`, `resep_user_id`, `like_id`) 
+(`resep_masakan_id`, `like_id`, `like_id_user`) 
 VALUES (?,?,?);";
 $stmt = $con->prepare($sql);
-$stmt->bind_param("sss", $id_masakan,$id_user_pembuat,$id_penyuka);
+$stmt->bind_param("sss", $resep_masakan_id, $like_id, $like_id_user);
 $stmt->execute();
 if ($stmt->affected_rows > 0) {
     $arr = ["result" => "success", "id" => $con->insert_id];
--- uas_kuremas/input_id_penyuka.php
+++ uas_kuremas/input_id_penyuka.php
@@ -5,10 +5,10 @@
 extract($_POST);
 $sql = "INSERT INTO `like` (`id_user`) VALUES (?)";
 $stmt = $con->prepare($sql);
-$stmt->bind_param("s", $id_penyuka);
+$stmt->bind_param("s", $id_user);
 $stmt->execute();
 if ($stmt->affected_rows > 0) {
-    $arr = ["result" => "success", "id" => $con->insert_id];
+    $arr = ["result" => "success", "like_id" => $con->insert_id, "like_id_user" => $id_user];
 } else {
     $arr = ["result" => "fail", "Error" => $con->error];
 }
